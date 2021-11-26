a3f8b4d1cf682a627fa654b1fc0df6292204e0e2
FelipeLenschow <53728705+FelipeLenschow@users.noreply.github.com>
2021-11-25 22:47:47 -0300

    Add files via upload

---
+++ servo_stm32/servo_stm32.ino
@@ -0,0 +1,55 @@
+
+HardwareTimer pwmtimer4(4);
+void setup() {
+//  pwmtimer4.pause();
+//  pwmtimer4.setOverflow(1440000);
+//  pwmtimer4.refresh();
+//  pwmtimer4.resume();
+
+    pwmtimer4.pause();
+    pwmtimer4.setPeriod(20000);
+    pwmtimer4.refresh();
+    pwmtimer4.resume();
+
+
+  pinMode(PA0, PWM);
+  pinMode(PC13, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+
+//  digitalWrite(PC13, HIGH);
+//  delayMicroseconds(1000);
+//  digitalWrite(PC13, LOW);
+//  delayMicroseconds(20000 - 1000);
+
+  //  ServoWrite(50);
+  //  delay(1000);
+  //  ServoWrite(150);
+  //  delay(1000);
+
+
+  //      for(int i=83;i<160;i++){
+  //        pwmWrite(PA0,i*1000);
+  //        Serial.println(i);
+  //        delay(500);
+  //      }
+
+    pwmWrite(PA0, 83000);
+    delay(2000);
+    pwmWrite(PA0, 120000);
+    delay(1000);
+
+}
+
+void ServoWrite(int Angle) {
+  int Time = map(constrain(Angle, 0, 180), 0, 180, 1000, 4200);
+  analogWrite(PC13, 255);
+  digitalWrite(PC13, HIGH);
+  delayMicroseconds(Time);
+  digitalWrite(PC13, LOW);
+  delayMicroseconds(20000 - Time);
+}
